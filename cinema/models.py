4768ae826d7e9720281edeaf60c798f78670de9b
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-22 16:09:32 +0300

    Added models

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,41 @@
 from django.db import models
+from rest_framework.fields import IntegerField
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField("Actor", related_name="movies")
+    genres = models.ManyToManyField("Genre", related_name="movies")
     duration = models.IntegerField()
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = IntegerField()
+
+    def __str__(self):
+        return (
+            f"{self.name}"
+            f"Rows: {self.rows}"
+            f"Seat in row: {self.seats_in_row}"
+        )
